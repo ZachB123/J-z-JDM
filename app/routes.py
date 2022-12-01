9877ebfa98f6a5326a573d2c0ccdb7823cea5379
Zach <zach.buchholz7@gmail.com>
2022-11-30 23:07:01 -0600

    created pages

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,28 @@ from app import app
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html", title="James is shit at remembering things")
+    return render_template("index.html")
+
+@app.route("/listings")
+def listings():
+    return render_template("listings.html")
+
+@app.route("/car")
+def car():
+    return render_template("car.html")
+
+@app.route("/account")
+def account():
+    return render_template("account.html")
+
+@app.route("/management")
+def management():
+    return render_template("management.html")
+
+@app.route("/loan")
+def loan():
+    return render_template("loan.html")
+
+@app.route("/control")
+def control():
+    return render_template("control.html")
