c56921a39a1c96253f7b533d030f02894f0d3c9d
jwod05 <jswia101@gmail.com>
2022-11-30 21:57:43 -0600

    setting up statics

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,4 @@ from app import app
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="James is shit at remembering things")
