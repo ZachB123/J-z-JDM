64fe8900e12e59ef213e59df8a9f95ce2b1312e8
jwod05 <jswia101@gmail.com>
2023-01-04 16:51:08 -0600

    loan calculator is functional

--- app/static/js/loan.js
+++ app/static/js/loan.js
@@ -4,47 +4,46 @@ let downPayment = 2000;
 let tradeIn = 0;
 let creditScore = .045;
 let loanTerm = 72;
-let cost = (((loanAmount - (downPayment + tradeIn))/loanTerm)*(1+creditScore));
+let cost = (((Number(loanAmount) - (Number(downPayment) + Number(tradeIn)))/Number(loanTerm))*(1+(Number(creditScore)/12)));
 
-document.querySelector("#priceSlider").addEventListener("change", loanSliderMatch);
-document.querySelector("#priceInput").addEventListener("change", loanInputMatch);
-document.querySelector("#downPaymentSelector").addEventListener("change", downPaymentMatch);
-document.querySelector("#tradeInSelector").addEventListener("change", tradeInMatch);
-document.querySelector("#creditScoreSelector").addEventListener("change", creditScoreMatch);
-document.querySelector("#termDropdownSelector").addEventListener("change", loanTermMatch);
+document.querySelector("#priceSlider").addEventListener("change", () => {loanSliderMatch(); loanCalculator(); });
+document.querySelector("#priceInput").addEventListener("change", () => {loanInputMatch(); loanCalculator();});
+document.querySelector("#downPaymentSelector").addEventListener("change", () => {downPaymentMatch(); loanCalculator();});
+document.querySelector("#tradeInSelector").addEventListener("change", () => {tradeInMatch(); loanCalculator();});
+document.querySelector("#creditScoreSelector").addEventListener("change", () => {creditScoreMatch(); loanCalculator();});
+document.querySelector("#termDropdownSelector").addEventListener("change", () => {loanTermMatch(); loanCalculator();});
 
 function loanSliderMatch() {
     sliderValue = document.querySelector("#priceSlider").value;
     loanAmount = Number(sliderValue);
     document.querySelector("#priceInput").value = '$'+sliderValue;
-    console.log("hello"); 
 }
 function loanInputMatch() {
     loanAmount = document.querySelector("#priceInput").value;
     sliderValue = loanAmount;
-    cost = (((loanAmount - (downPayment + tradeIn))/loanTerm)*(1+creditScore));
 }
 function downPaymentMatch() {
     downPayment = document.querySelector("#downPaymentSelector").value;
-    cost = (((loanAmount - (downPayment + tradeIn))/loanTerm)*(1+creditScore));
 }
 function tradeInMatch() {
     tradeIn = document.querySelector("#tradeInSelector").value;
-    cost = (((loanAmount - (downPayment + tradeIn))/loanTerm)*(1+creditScore));
 }
 function creditScoreMatch() {
     creditScore = document.querySelector("#creditScoreSelector").value;
-    cost = (((loanAmount - (downPayment + tradeIn))/loanTerm)*(1+creditScore));
 }
 function loanTermMatch() {
     loanTerm = document.querySelector("#termDropdownSelector").value;
-    cost = (((loanAmount - (downPayment + tradeIn))/loanTerm)*(1+creditScore));
 }
 
-document.querySelector(".input-boxes").addEventListener("change", loanCalculator);
+
 
 function loanCalculator() {
-    cost = (((loanAmount - (downPayment + tradeIn))/loanTerm)*(1+creditScore));
-    document.querySelector("#monthPriceSelector").innerHTML = cost.toString();
-    console.log("Hello")
+    cost = (((Number(loanAmount) - (Number(downPayment) + Number(tradeIn)))/Number(loanTerm))*(1+(Number(creditScore)/12)));
+    document.querySelector("#monthPriceSelector").textContent = '$'+String(Math.trunc(cost));
+    document.querySelector("#budgetAmountSelector").textContent = '$'+String(Math.trunc(loanAmount));
+    document.querySelector("#downPaymentInfoSelector").textContent = '-$'+String(Math.trunc(downPayment));
+    document.querySelector("#tradeValueSelector").textContent = '$'+String(Math.trunc(tradeIn));
+    document.querySelector("#estimateAprSelector").textContent = String(Math.trunc(creditScore*100))+'%';
+    document.querySelector("#totalAmountSelector").textContent = '$'+String(Math.trunc(Number(cost)*Number(loanTerm)));
+    document.querySelector("#monthPriceSelectorBottom").textContent = '$'+String(Math.trunc(cost));
 }
