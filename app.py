4e3c14b5833f48bb72ec41f8858d5ef7075cae15
Zach <zach.buchholz7@gmail.com>
2023-01-05 17:02:58 -0600

    registration

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import os
 from config import Config
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from dotenv import load_dotenv
 load_dotenv()
-from forms import LoginForm
-from flask_login import LoginManager, current_user, login_user, logout_user
+from forms import LoginForm, RegistrationForm
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 from models import User
+from werkzeug.urls import url_parse
+from database import db
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 login = LoginManager(app)
-login.login_view = "Login"
+login.login_view = "login"
 # login.init_app(app)
 
 @login.user_loader
@@ -34,19 +36,32 @@ def login():
             flash("Invalid username or password")
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for("index"))
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for("index")
+        return redirect(next_page)
     return render_template("login.html", form=form)
 
 @app.route("/logout")
 def logout():
     logout_user()
+    # TODO make it so you return to the page you were on
     return redirect(url_for("index"))
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    pass
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        db.create_user(User(form.username.data, form.email.data, form.password.data))
+        flash("Cogratulations, you are now a registered user!")
+        return redirect(url_for("login"))
+    return render_template("register.html", form=form)
+
 
 @app.route("/profile")
+@login_required
 def profile():
     return render_template("profile.html")
 
@@ -58,10 +73,6 @@ def listings():
 @app.route("/car")
 def car():
     return render_template("car.html")
-
-@app.route("/account")
-def account():
-    return render_template("account.html")
 
 @app.route("/management")
 def management():
