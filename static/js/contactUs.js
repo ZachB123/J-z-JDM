af97fbdb4a8c6fac48f3bcc0f1a308f3954ecc5b
Zach <zach.buchholz7@gmail.com>
2023-03-20 19:54:28 -0500

    faq js

--- static/js/contactUs.js
+++ static/js/contactUs.js
@@ -1,34 +1,50 @@
 
-document.getElementById('contactUsButtonFlip').addEventListener("click", () => {
-    document.getElementById('contactUsFormFront').classList.add("group-rotate-y-180");
-    console.log("Nuts");
-    let element = document.getElementById('contactUsFormFront');
-    console.log(element);
-    console.log(element.classList);
-});
-let element = document.getElementById('contactUsFormFront');
-console.log(element);
-console.log(element.classList);
+// document.getElementById('contactUsButtonFlip').addEventListener("click", () => {
+//     document.getElementById('contactUsFormFront').classList.add("group-rotate-y-180");
+//     console.log("Nuts");
+//     let element = document.getElementById('contactUsFormFront');
+//     console.log(element);
+//     console.log(element.classList);
+// });
+// let element = document.getElementById('contactUsFormFront');
+// console.log(element);
+// console.log(element.classList);
 // install tailwind nesting plugin
 
 
 
-
-
-
 // JS for the collapsible FAQ
-let coll = document.getElementsById("collapsibleFAQ");
-let i;
+// let coll = document.getElementsById("collapsibleFAQ");
+// let i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    // this.classList.toggle("active");
-    // let content = this.nextElementSibling;
-    // if (content.style.display === "block") {
-    //   content.style.display = "hidden";
-    // } else {
-    //   content.style.display = "block";
-    // }
-    console.log("This");
-  });
-} 
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     // this.classList.toggle("active");
+//     // let content = this.nextElementSibling;
+//     // if (content.style.display === "block") {
+//     //   content.style.display = "hidden";
+//     // } else {
+//     //   content.style.display = "block";
+//     // }
+//     console.log("This");
+//   });
+// } 
+
+let buttons = dqa(".faq-box button")
+console.log(buttons)
+
+function getRevealFunction(elem) {
+  return () => {
+    elem.nextElementSibling.classList.toggle("hidden")
+  }
+}
+
+for (let button of buttons) {
+  button.addEventListener("click", getRevealFunction(button))
+}
+
+// let content = dqa(".FAQ-content")
+// for (let box of content) {
+//   box.classList.toggle("hidden")
+//   box.classList.toggle("hidden")  
+// }
